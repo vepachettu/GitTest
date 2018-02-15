eaf78457e2b46b00e35a88198096763f4408e5a3
vepachettu <s.2017.and@gmail.com>
2018-02-15 18:49:48 -0500

    test 2

--- app/src/main/java/com/siri_android/gittest/MainActivity.java
+++ app/src/main/java/com/siri_android/gittest/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // hi this is test app for git 
+        // hi this is test app for git
     }
 }
